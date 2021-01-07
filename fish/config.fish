d9aef3b3ac1f9b2b5da476e6e7b8291d68bc24b2
siv8510 <shibuuuu5@gmail.com>
2021-01-08 02:35:28 +0900

    add alias

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+alias w='cd ~/workspace'
+
+
+alias dcd='docker-compose down'
+alias dce='docker-compose exec'
+alias dcp='docker-compose ps'
+alias dcud='docker-compose up -d'
+alias dcubd='docker-compose up --build -d'
+alias di='docker images'
+alias dp='docker ps'
+alias dr='docker rm'
+alias drmi='docker rmi'
+alias dv='docker volume ls'
+alias dvr='docker volume rm'
+
+alias g='git'

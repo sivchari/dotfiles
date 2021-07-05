921485d74de3956fe1457690be4ebc9b6e3a0004
sivchari <shibuuuu5@gmail.com>
2021-07-05 11:11:59 +0900

    modify installer

--- dotfile-installer.sh
+++ dotfile-installer.sh
@@ -68,6 +68,7 @@ echo '***************************************************'
 echo '***************************************************'
 echo 'INSTALL ~> fonts'
 echo '***************************************************'
+brew tap homebrew/cask-fonts
 brew install font-hack-nerd-font
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 echo '***************************************************'

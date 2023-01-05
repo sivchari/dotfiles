0dc4faf0304d4a310c5a9444257619e07f736296
sivchari <shibuuuu5@gmail.com>
2023-01-05 15:46:02 +0900

    feat: add back keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<return><return>', '<c-w><c-w>', { silent = true })
+vim.keymap.set('n', '<space><space>', '<c-o>', { silent = true })
 vim.keymap.set('i', '<delete>', '<esc>', { silent = true })

746c1017b726bb993c21c02fd18be98d210fea68
sivchari <shibuuuu5@gmail.com>
2024-06-16 13:42:37 +0900

    run stylua
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap
 
-keymap.set('n', '<space><space>', '<c-o>', { silent = true })
-keymap.set('i', '<delete>', '<esc>', { silent = true })
-keymap.set('n', '<delete>', 's', { silent = true })
+keymap.set("n", "<space><space>", "<c-o>", { silent = true })
+keymap.set("i", "<delete>", "<esc>", { silent = true })
+keymap.set("n", "<delete>", "s", { silent = true })
--- lua/plugins_config.lua
+++ lua/plugins_config.lua
@@ -1,24 +1,23 @@
 local keymap = vim.keymap
 
 -- nvim-tree
-keymap.set('n', '<c-q>', ':NvimTreeOpen<cr>', { silent = true })
-keymap.set('n', '<c-c>', ':NvimTreeClose<cr>', { silent = true })
+keymap.set("n", "<c-q>", ":NvimTreeOpen<cr>", { silent = true })
+keymap.set("n", "<c-c>", ":NvimTreeClose<cr>", { silent = true })
 
 -- telescope
-keymap.set('n', 'tg', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
-keymap.set('n', 'tf', '<cmd>lua require("telescope.builtin").find_files()<cr>')
+keymap.set("n", "tg", '<cmd>lua require("telescope.builtin").live_grep()<cr>')
+keymap.set("n", "tf", '<cmd>lua require("telescope.builtin").find_files()<cr>')
 
 -- easymotion
-keymap.set('n', '<leader>c', ':HopChar1<cr>', { silent = true })
-keymap.set('n', '<leader>w', ':HopWord<cr>', { silent = true })
-keymap.set('n', '<leader>l', ':HopLine<cr>', { silent = true })
+keymap.set("n", "<leader>c", ":HopChar1<cr>", { silent = true })
+keymap.set("n", "<leader>w", ":HopWord<cr>", { silent = true })
+keymap.set("n", "<leader>l", ":HopLine<cr>", { silent = true })
 
 -- lazygit
-keymap.set('n', '<leader>g', ':LazyGit<cr>')
+keymap.set("n", "<leader>g", ":LazyGit<cr>")
 
 -- color scheme
-vim.cmd('colorscheme github_dark')
+vim.cmd("colorscheme github_dark")
 
 -- copilot
 vim.g.copilot_filetypes = { markdown = true, gitcommit = true, yaml = true }
-

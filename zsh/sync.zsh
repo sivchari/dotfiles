98bd6afc0eef183dadc0d249c76d93cc0afb5e3a
sivchari <shibuuuu5@gmail.com>
2024-11-19 10:42:54 +0900

    move func to sync
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- zsh/sync.zsh
+++ zsh/sync.zsh
@@ -42,5 +42,13 @@ function plan9port() {
   export PATH=$HOME/plan9port/bin:$PATH
 }
 
+function gr() {
+    FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --reverse --height=50%"
+    local root="$HOME/workspace"
+    local repo="$(cat ~/workspace/sivchari/dotfiles/ghq_list.txt | fzf --preview="ls -AF --color=always ${root}/{1}")"
+    local dir="${root}/${repo}"
+    [ -n "${dir}" ] && cd "${dir}"
+}
+
 setopt PROMPT_SUBST
 precmd_functions+=(set_prompt)
